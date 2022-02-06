12f92ef5f1f12e6972018bbbf4fc5e696074bd1d
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2022-02-06 09:49:41 +0530

    Header - Brand and Connect wallet button

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,5 +1,45 @@
+import styled from "styled-components";
+
 const Connect = () => {
-    return <div>Connect</div>;
+    return (
+        <ConnectContainer>
+            <Header>
+                <Brand>Tulip</Brand>
+                <Button>Connect Wallet</Button>
+            </Header>
+        </ConnectContainer>
+    );
 };
+
+const ConnectContainer = styled.div``;
+const Header = styled.div`
+    height: 4rem;
+    display: flex;
+    border-bottom: 1px solid lightgray;
+    justify-content: space-between;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 20px;
+    box-shadow: 0 3px 3px lightgray;
+`;
+const Brand = styled.div`
+    font-size: 30px;
+    font-weight: bolder;
+    margin-left: 80px;
+    font-family: verdana;
+`;
+const Button = styled.div`
+    height: 40px;
+    background: blue;
+    color: white;
+    display: flex;
+    width: max-content;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+    box-sizing: border-box;
+    cursor: pointer;
+    border-radius: 5px;
+`;
 
 export default Connect;
